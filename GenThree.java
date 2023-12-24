ca1650af59bc87ea2ab168c0d5ff714c0c191c65
Linoy Gorlik <154327058+linoy-gorlik@users.noreply.github.com>
2023-12-24 19:24:23 +0200

    Add files via upload

--- GenThree.java
+++ GenThree.java
@@ -8,17 +8,40 @@ public class GenThree {
 		int b = Integer.parseInt(args[1]);
 
     int range = b-a;
-	for (int x = 0; x<3; x++){
-	int rand = (int)(Math.random() * range + a);
-	System.out.println(rand);
 	
-	}
+	int rand = (int)(Math.random() * range + a);
+	int rand1 = (int)(Math.random() * range + a);
+	int rand2 = (int)(Math.random() * range + a);
+	System.out.println(rand);
+	System.out.println(rand1);
+	System.out.println(rand2);
+
+		int smallest = Math.min(Math.min(rand, rand1), Math.min(rand1,rand2));
+System.out.println("The minimal generated number was " + smallest);
+
 
 
 	
 	   
-	  
-	
+	   { 
+		// System.out.println("mistake");
+		// System. exit(0);
+
+	   }
+	//   if ((b < c) | (b < d) | (b < e))
+      {
+		// System.out.println("cant");
+		// System. exit(0);
+	}
+
+    //  int small =Math.min(c, d);
+	//  int smallest = Math.min(small,e);
+	//  System.out.println(smallest);
+
+	//  System.out.println(c);
+	//  System.out.println(d);
+	//  System.out.println(e);
+	//  System.out.println("The minimal generated number was " + smallest);
 
 	}	
 	
